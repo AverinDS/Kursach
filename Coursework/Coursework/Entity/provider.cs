311f653a27fbe4fa6837781f3ad90df019eede5e
AverinDS <awerin960@yandex.ru>
2016-03-17 18:52:59 +0300

    Созданы основные классы, но поля заполнены не до конца

---
+++ Coursework/Coursework/Entity/provider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coursework
+{
+    class provider//поставщик
+    {
+        public int ID;
+        public string Name;
+        public string currensy; //валюта
+    }
+}
