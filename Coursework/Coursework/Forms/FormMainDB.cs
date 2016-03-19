627f733025ed689ab720f6b4124e11e4f225c756
AverinDS <awerin960@yandex.ru>
2016-03-19 17:53:14 +0300

    Изменен gitignore чтобы добавить sqlite

--- Coursework/Coursework/Forms/FormMainDB.cs
+++ Coursework/Coursework/Forms/FormMainDB.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
 
 namespace Coursework
 {
@@ -15,6 +17,18 @@ namespace Coursework
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(@"C:\Database.sqlite"))//попытка найти БД. если её нет, создаём новую
+            {
+                SQLiteConnection.CreateFile(@"C:\Database.sqlite");
+                SQLiteConnection connect = new SQLiteConnection(@"Data Source = C:\Database.sqlite; Version = 3");
+                SQLiteCommand command = new SQLiteCommand(
+                    "create table product(id integer primary key, name varchar(20)) ", connect);
+            }
+
         }
     }
 }
